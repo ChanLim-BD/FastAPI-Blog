292616607badd23562f8ef8bf32375885f93a697
Chan_Lim <linsay070@gmail.com>
2025-02-12 09:48:33 +0900

    Image Upload 적용

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes import blog
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(blog.router)
 
 @app.get("/")
--- routes/blog.py
+++ routes/blog.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Request, Depends, Form, status
-from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Request, Depends, Form, UploadFile, File, status
+from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
 from db.database import direct_get_conn, context_get_conn
@@ -8,7 +8,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from schemas.blog_schema import Blog, BlogData
 from services import blog_svc
 from utils import util
-
 
 
 # router 생성
@@ -36,6 +35,7 @@ def get_blog_by_id(request: Request, id: int,
         request = request,
         name="show_blog.html",
         context = {"blog": blog})
+
  
 @router.get("/new")
 def create_blog_ui(request: Request):
@@ -50,9 +50,17 @@ def create_blog(request: Request
                 , title = Form(min_length=2, max_length=200)
                 , author = Form(max_length=100)
                 , content = Form(min_length=2, max_length=4000)
+                , imagefile: UploadFile | None = File(None)
                 , conn: Connection = Depends(context_get_conn)):
     
-    blog_svc.create_blog(conn, title=title, author=author, content=content)
+    image_loc = None
+    if len(imagefile.filename.strip()) > 0:
+        # 반드시 transactional 한 처리를 위해 upload_file()이 먼저 수행되어야 함.
+        image_loc = blog_svc.upload_file(author=author, imagefile=imagefile)
+        blog_svc.create_blog(conn, title=title, author=author, content=content, image_loc=image_loc)
+    else:
+        blog_svc.create_blog(conn, title=title, author=author, content=content, image_loc=image_loc)
+
 
     return RedirectResponse("/blogs", status_code=status.HTTP_302_FOUND)
     
@@ -66,20 +74,29 @@ def update_blog_ui(request: Request, id: int, conn = Depends(context_get_conn)):
         name="edit_blog.html",
         context = {"blog": blog}
     )
+
     
 @router.post("/edit/{id}")
 def update_blog(request: Request, id: int
                 , title = Form(min_length=2, max_length=200)
                 , author = Form(max_length=100)
                 , content = Form(min_length=2, max_length=4000)
+                , imagefile: UploadFile | None = File(None)
                 , conn: Connection = Depends(context_get_conn)):
     
-    blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content)
+    image_loc = None
+    if len(imagefile.filename.strip()) > 0:
+        image_loc = blog_svc.upload_file(author=author, imagefile=imagefile)
+        blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
+    else:
+        blog_svc.update_blog(conn=conn, id=id, title=title, author=author, content=content, image_loc=image_loc)
     return RedirectResponse(f"/blogs/show/{id}", status_code=status.HTTP_302_FOUND)
     
+
 @router.delete("/delete/{id}")
 def delete_blog(request: Request, id: int
                 , conn: Connection = Depends(context_get_conn)):
-    
-    blog_svc.delete_blog(conn=conn, id=id)
+    blog = blog_svc.get_blog_by_id(conn=conn, id=id)
+    blog_svc.delete_blog(conn=conn, id=id, image_loc=blog.image_loc)
+    return JSONResponse(content="메시지가 삭제되었습니다", status_code=status.HTTP_200_OK)
     # return RedirectResponse("/blogs", status_code=status.HTTP_302_FOUND)
